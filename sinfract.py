e117b03a33dfe1563b22c31e20aa96aeb47d2655
Federico Bonfiglio <federico.bonfiglio@protonmail.ch>
2022-03-04 16:39:48 +0100

    iterative version

--- sinfract.py
+++ sinfract.py
@@ -6,8 +6,16 @@ def sinfract(x, a, f, n, k=4):
     if n == 0:
         return 0
     return a*np.sin(2*np.pi*f*x) + sinfract(x, a/k, f*k, n-1, k=k)
+
+def sinfractl(x, a, f, n, k=4):
+    y = np.zeros(len(x))
+    for i in range(0, int(n)):
+        y += a*np.sin(2*np.pi*f*x)
+        f = f*k
+        a = a/k
+    return y
             
-x = np.linspace(0, 10, 10000)
+x = np.linspace(0, 1000, 100000)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -23,7 +31,7 @@ n = float(sys.argv[2])
 k = float(sys.argv[3])
 
 # plot the function
-plt.plot(x,sinfract(x, 1, 1/period, n, k), 'b-')
+plt.plot(x,sinfractl(x, 1, 1/period, n, k), 'b-')
 
 # show the plot
 plt.show()
