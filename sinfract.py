f12c0e3ce6788356c5cab98c026b89de6506ffb8
Federico Bonfiglio <federico.bonfiglio@protonmail.ch>
2022-03-04 16:21:18 +0100

    amplitude

--- sinfract.py
+++ sinfract.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-def sinfract(x, f, n, k=4):
+def sinfract(x, a, f, n, k=4):
     if n == 0:
         return 0
-    return np.sin(2*np.pi*f*x) + (1/k)*sinfract(x, f*k, n-1, k)
+    return a*np.sin(2*np.pi*f*x) + sinfract(x, a/k, f*k, n-1, k=k)
             
-x = np.linspace(-np.pi, np.pi, 10000)
+x = np.linspace(0, 10, 10000)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -17,12 +18,12 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-freq = 1/6
-n = 30
+period = float(sys.argv[1])
+n = float(sys.argv[2])
+k = float(sys.argv[3])
 
 # plot the function
-#plt.plot(x,sinfract(x, freq, 1), 'b-')
-plt.plot(x,sinfract(x, freq, n, 4), 'b-')
+plt.plot(x,sinfract(x, 1, 1/period, n, k), 'b-')
 
 # show the plot
 plt.show()
